2abdd8ce46f6b0f3a67fa99b5f0a8d8f5b876c6c
Fuze95 <60931780+Fuze95@users.noreply.github.com>
2024-11-16 14:16:07 +0530

    Create note.dart

---
+++ lib/models/note.dart
@@ -0,0 +1,56 @@
+enum NoteStatus {
+  active,
+  archived
+}
+
+class Note {
+  final int? id;
+  String title;
+  String content;
+  DateTime createdAt;
+  DateTime modifiedAt;
+  String? label;
+  List<String> categories;
+  String? imagePath;
+  NoteStatus status;
+
+  Note({
+    this.id,
+    required this.title,
+    required this.content,
+    required this.createdAt,
+    required this.modifiedAt,
+    this.label,
+    this.categories = const [],
+    this.imagePath,
+    this.status = NoteStatus.active,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'created_at': createdAt.toIso8601String(),
+      'modified_at': modifiedAt.toIso8601String(),
+      'label': label,
+      'categories': categories.join(','),
+      'image_path': imagePath,
+      'status': status.index,
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'],
+      title: map['title'],
+      content: map['content'],
+      createdAt: DateTime.parse(map['created_at']),
+      modifiedAt: DateTime.parse(map['modified_at']),
+      label: map['label'],
+      categories: map['categories']?.split(',') ?? [],
+      imagePath: map['image_path'],
+      status: NoteStatus.values[map['status'] ?? 0],
+    );
+  }
+}
