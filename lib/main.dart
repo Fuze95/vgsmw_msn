dc3259617d7380cc386fc168fa2038196d6f0bea
Fuze95 <60931780+Fuze95@users.noreply.github.com>
2024-11-16 14:15:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:vgsmw_msn/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+import 'database/database_handler.dart';
+import 'providers/note_provider.dart';
+import 'providers/theme_provider.dart';
+import 'providers/loading_provider.dart';
+import 'screens/splash_screen.dart';
+import 'utils/theme.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final databaseHandler = DatabaseHandler();
+  await databaseHandler.initDatabase();
 
-void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => NoteProvider(databaseHandler)),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => LoadingProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: const HomeScreen(),
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          title: 'MySimpleNote',
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+          home: const SplashScreen(),
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
